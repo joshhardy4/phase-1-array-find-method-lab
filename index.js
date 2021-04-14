8a4fda5d2de5b6a0ac8cb3dc42f3757220c32f40
Josh <leonidus00@yahoo.com>
2021-04-14 15:19:24 -0500

    commit tree 4eaabf59fbb4b301c10610c4e4916dd2356ead20

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+function superbowlWin(record){
+    
+    const result = record.find(win => (win.result === 'W'));
+    console.log(result);
+    
+        if (result)
+        return result.year;
+    }  
+    
 
+
+    
+
+    
